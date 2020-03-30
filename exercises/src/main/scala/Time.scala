93aada096f3702aa269b4ed03ce3d77fd04d56ae
lmartinez <lmartinezj@gmail.com>
2020-03-30 02:52:12 -0500

    created method minus that takes a parameter of type time and takes the difference with the object time asMinutes

--- exercises/src/main/scala/Time.scala
+++ exercises/src/main/scala/Time.scala
@@ -2,4 +2,5 @@ class Time(val hours: Int, val minutes: Int) {
   val asMinutes: Int = (hours * 60) + minutes;
   // TODO Verify that hours is within 0 and 23
   //  TODO Verify that minutes is within 0 and 59
+  def minus(that:Time):Int = asMinutes - that.asMinutes
 }
