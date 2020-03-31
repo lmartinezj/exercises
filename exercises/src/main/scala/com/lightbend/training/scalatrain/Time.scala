9b5c1ffa501df80d457cc613e12383011efa525d
lmartinez <lmartinezj@gmail.com>
2020-03-31 01:58:11 -0500

    moved classes Time and Train to com.lightbend.training.scalatrain changed loadInitialCmds in AdditionalSettings.scala to "true"

--- exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
@@ -1,3 +1,5 @@
+package com.lightbend.training.scalatrain
+
 class Time(val hours: Int = 0, val minutes: Int = 0) {
   val asMinutes: Int = (hours * 60) + minutes;
   // TODO Verify that hours is within 0 and 23
