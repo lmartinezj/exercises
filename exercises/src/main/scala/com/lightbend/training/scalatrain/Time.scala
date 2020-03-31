6add89612c76f3276f8ff34e41eb4c9c9bd0922b
lmartinez <lmartinezj@gmail.com>
2020-03-31 18:33:50 -0500

    created a singleton Time object and added fromMinutes methods to the object

--- exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
@@ -1,9 +1,19 @@
 package com.lightbend.training.scalatrain
 
 class Time(val hours: Int = 0, val minutes: Int = 0) {
-  val asMinutes: Int = (hours * 60) + minutes;
+  val asMinutes: Int = (hours * 60) + minutes
   // TODO Verify that hours is within 0 and 23
   //  TODO Verify that minutes is within 0 and 59
+
   def minus(that:Time):Int = asMinutes - that.asMinutes
+
   def -(that:Time):Int = minus(that)
+}
+
+object Time {
+  def fromMinutes(minutes: Int):Time = {
+    val  myHours:Int = minutes / 60
+    val myMinutes:Int = minutes % 60
+    new Time(myHours, myMinutes)
+  }
 }
