c7b389bf6bf87d11e1478bda710d9c6ee4af40b9
lmartinez <lmartinezj@gmail.com>
2020-04-01 00:56:16 -0500

    created preconditions (require) for hours between 0-23 and minutes between 0-59

--- exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
@@ -2,11 +2,9 @@ package com.lightbend.training.scalatrain
 
 class Time(val hours: Int = 0, val minutes: Int = 0) {
   val asMinutes: Int = (hours * 60) + minutes
-  // TODO Verify that hours is within 0 and 23
-  //  TODO Verify that minutes is within 0 and 59
-
+  require(hours>=0 && hours<=23, "hour value out of permitted range")
+  require(minutes>=0 && minutes<=59, "minute value out of permitted range")
   def minus(that:Time):Int = asMinutes - that.asMinutes
-
   def -(that:Time):Int = minus(that)
 }
 
