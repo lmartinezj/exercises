73e5a65f8be01b2c362785b5a3b5669f90e23dfc
lmartinez <lmartinezj@gmail.com>
2020-04-03 01:37:50 -0500

    Added JourneyPlanner.scala takes all trains and its stations. Added test

--- exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
@@ -4,7 +4,6 @@ package com.lightbend.training.scalatrain
 case class Train(kind: String, number: Int, schedule: Seq[(Time,Station)]) {
   require(schedule.size >= 2, "Schedule must contain at least 2 elements")
   val stations: Seq[Station] = schedule.map(_._2)
-  //this.copy(stations = schedule.map(_._2))
   }
 
 
