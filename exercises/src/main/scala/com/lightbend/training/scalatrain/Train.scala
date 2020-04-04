e255805ebdfaadaea7352749f7d9a73f517901a0
lmartinez <lmartinezj@gmail.com>
2020-04-04 18:56:57 -0500

    Created TrainInfo.scala abstract class with InterCityExpress, RegionalExpress and BavarianRegionalExpress as case classes inheriting from TrainInfo.scala. Replaced Train.scala parameters kind and number with TrainInfo.scala class parameter. Added tests

--- exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
@@ -1,7 +1,7 @@
 package com.lightbend.training.scalatrain
 
 
-case class Train(kind: String, number: Int, schedule: Seq[(Time,Station)]) {
+case class Train(info: TrainInfo, schedule: Seq[(Time,Station)]) {
   require(schedule.size >= 2, "Schedule must contain at least 2 elements")
   val stations: Seq[Station] = schedule.map(_._2)
   }
---
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/TrainInfo.scala
@@ -0,0 +1,9 @@
+package com.lightbend.training.scalatrain
+
+sealed abstract class TrainInfo {
+  def number:Int
+}
+
+case class InterCityExpress(number:Int, hasWifi: Boolean = false) extends TrainInfo
+case class RegionalExpress(number: Int) extends TrainInfo
+case class BavarianRegionalExpress(number: Int) extends TrainInfo
