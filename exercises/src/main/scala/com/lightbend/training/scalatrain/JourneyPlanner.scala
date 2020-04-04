903bd053e9b741099908db6b0e8b6ac105c73547
lmartinez <lmartinezj@gmail.com>
2020-04-04 03:14:39 -0500

    Created method stopsAt in JourneyPlanner.scala. Added tests

--- exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
@@ -3,4 +3,9 @@ package com.lightbend.training.scalatrain
 class JourneyPlanner(trains: Set[Train]) {
   val stations: Set[Station] = trains.flatMap(_.stations)
   def trainsAt(station: Station): Set[Train] = trains.filter(_.stations.contains(station))
+  def stopsAt(station: Station): Set[(Time, Train)] =
+    for {
+      train <- trains
+      stop <- train.schedule if stop._2 == station
+    } yield (stop._1 -> train)
 }
