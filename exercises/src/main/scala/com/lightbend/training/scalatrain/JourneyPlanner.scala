5a2b711182a3179019f2b7459bb0fb2641f0dec8
lmartinez <lmartinezj@gmail.com>
2020-04-05 04:08:48 -0500

    Added isShortTrip method to JourneyPlanner.scala

--- exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/JourneyPlanner.scala
@@ -8,4 +8,12 @@ class JourneyPlanner(trains: Set[Train]) {
       train <- trains
       stop <- train.schedule if stop._2 == station
     } yield (stop._1 -> train)
+
+  def isShortTrip(from: Station, to: Station): Boolean = {
+    trains.exists(_.stations.dropWhile(_ != from) match {
+      case `from` +: `to` +: _ => true
+      case `from` +: _ +: `to` +: _ => true
+      case _ => false
+    })
+  }
 }
